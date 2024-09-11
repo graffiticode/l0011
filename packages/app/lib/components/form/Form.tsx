303ba6e37f4a207ede554b9c616d53040b780f25
Jeff Dyer <jeffdyer@acm.org>
2024-09-11 16:40:54 -0700

    Modernize

--- packages/app/lib/components/form/Form.tsx
+++ packages/app/lib/components/form/Form.tsx
@@ -1,51 +1,214 @@
-import "../../index.css";
-import { useState, useEffect } from "react";
-
-import { ThemeToggle } from "./ThemeToggle";
+import React, { useState, useEffect, useRef } from 'react';
+import { Switch } from '@headlessui/react'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+import { Combobox } from '@headlessui/react'
+React;
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-function renderJSON(data) {
-  delete data.schema;
-  delete data.theme;
+function Text({ value, onChange }) {
+  const [ currentValue, setCurrentValue ] = useState(value);
   return (
-    <pre className="text-xs">{JSON.stringify(data, null, 2)}</pre>
-  );
+    <div>
+      <label htmlFor="email" className="sr-only">
+        Text
+      </label>
+      <input
+        type="text"
+        name="text"
+        id="text"
+        className="block w-full rounded-none border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6 px-3 focus:outline-none"
+        defaultValue={currentValue}
+        onChange={(e) => setCurrentValue(e.target.value)}
+        onBlur={() => onChange(currentValue)}
+      />
+    </div>
+  )
 }
 
-function render({ state }) {
-  const { data } = state;
-  if (typeof data?.hello === "string") {
-    return <span className="text-sm">{`hello, ${data.hello}!`}</span>;
-  } else if (typeof data.image === "string") {
-    console.log("render() data=" + JSON.stringify(data, null, 2));
-    return <img src={data.image} />;
-  } else {
-    return renderJSON(data);
-  }
+function Toggle({ disabled, value: enabled, onChange }) {
+  const [checked, setChecked] = useState(enabled)
+  useEffect(() => {
+    setChecked(enabled);
+  }, [enabled]);
+  useEffect(() => {
+    onChange(checked);
+  }, [checked]);
+  return (
+    <Switch.Group as="div" className="flex items-center">
+      <Switch
+        checked={checked}
+        disabled={disabled}
+        onChange={setChecked}
+        className={classNames(
+          checked ? 'bg-gray-600' : 'bg-gray-200',
+          'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-none border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none'
+        )}
+      >
+        <span
+          aria-hidden="true"
+          className={classNames(
+            checked ? 'translate-x-5' : 'translate-x-0',
+            'pointer-events-none inline-block h-5 w-5 transform rounded-none bg-white shadow ring-0 transition duration-200 ease-in-out'
+          )}
+        />
+      </Switch>
+    </Switch.Group>
+  )
+}
+
+const optionsFromList = list => list.map((name, id) => ({id, name}));
+
+function Combo({value = "", list, onChange}) {
+  const [ options ] = useState(optionsFromList(list));
+  const [ query, setQuery ] = useState('')
+  const [ selectedOption, setSelectedOption ] = useState(options.find(option => option.name === value))
+  const filteredOptions =
+    query === ''
+      ? options
+      : options.filter((option) => {
+          return option.name.toLowerCase().includes(query.toLowerCase())
+      })
+  useEffect(() => {
+    if (selectedOption) {
+      onChange(selectedOption.name);
+    }
+  }, [selectedOption]);
+  return (
+    <Combobox as="div" value={selectedOption} onChange={setSelectedOption}>
+      <div className="relative mt-2">
+        <Combobox.Input
+          className="w-full rounded-none border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6 focus:outline-none"
+          onChange={
+            (event) => (
+              setQuery(event.target.value)
+            )
+          }
+          displayValue={
+            (option: any) => option.name
+          }
+        />
+        <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+          <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+        </Combobox.Button>
+
+        {filteredOptions.length > 0 && (
+          <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-none bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            {filteredOptions.map((option) => (
+              <Combobox.Option
+                key={option.name}
+                value={option}
+                className={({ active }) =>
+                  classNames(
+                    'z-100 relative cursor-default select-none py-2 pl-3 pr-9',
+                    active ? 'bg-gray-600 text-white' : 'text-gray-900'
+                  )
+                }
+              >
+                {({ active, selected }) => (
+                  <>
+                    <span
+                      key="1"
+                      className={
+                        classNames('block truncate', selected && 'font-semibold')}>
+                      {option.name}
+                    </span>
+                    {selected && (
+                      <span
+                        key="2"
+                        className={classNames(
+                          'absolute inset-y-0 right-0 flex items-center pr-4',
+                          active ? 'text-white' : 'text-gray-600'
+                        )}
+                      >
+                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                      </span>
+                    )}
+                  </>
+                )}
+              </Combobox.Option>
+            ))}
+          </Combobox.Options>
+        )}
+      </div>
+    </Combobox>
+  )
+}
+
+function Props({ state }) {
+  const data = state.data;
+  const schema = data.schema;
+  const propDefs = schema?.properties || {};
+  const fields = Object.keys(propDefs).map(key => {
+    if (data[key] === undefined) {
+      return undefined;
+    }
+    const propDef = propDefs[key];
+    return {
+      name: key,
+      desc: propDef.description,
+      type: propDef.type,
+      input: {type: "Text", values: data[key] || ""},
+    };
+  }).filter(field => field !== undefined);
+  console.log("L0011 Props() fields=" + JSON.stringify(fields, null, 2));
+  const handleChange = args => {
+    state.apply({
+      type: "change",
+      args,
+    });
+  };
+  return (
+    <div className="p-2">
+      <div className="px-4 py-6 font-light grid grid-cols-1 md:grid-cols-5 text-sm">
+        {
+          fields.map((field) => {
+            const propDef = propDefs[field.name];
+            return (
+              <>
+                <div key={field.name + "1"} className="font-mono pt-4 md:pt-0">{field.name}</div>
+                <div key={field.name + "2"} className="col-span-1 md:col-span-3 text-gray-500 pb-2">{field.desc}<br/><span className="font-mono p-1 rounded-none bg-gray-100 text-xs">{field.type}</span></div>
+                <div key={field.name + "3"} className="col-span-1">
+                  {
+                    propDef.enum &&
+                      <Combo
+                        list={propDef.enum}
+                        value={state.data[field.name]}
+                        onChange={(value) => handleChange({[field.name]: value})}
+                      /> ||
+                      propDef.type === "boolean" &&
+                      <Toggle
+                        disabled={false}
+                        value={state.data[field.name]}
+                        onChange={(value) => handleChange({[field.name]: value})}
+                      /> ||
+                      <Text
+                        value={field.input.values}
+                        onChange={(value) => handleChange({[field.name]: value})}
+                      />
+                  }
+                </div>
+              </>
+            );
+          })
+        }
+      </div>
+    </div>
+  )
 }
 
 export const Form = ({ state }) => {
-  const [ theme, setTheme ] = useState(state.data.theme || "light");
-  useEffect(() => {
-    state.apply({
-      type: "change",
-      args: {
-        theme,
-      },
-    });
-  }, [theme]);
+  const ref = useRef();
+  // useEffect(() => {
+  //   if (ref?.current?.offsetHeight) {
+  //     setHeight(ref.current?.offsetHeight);
+  //   }
+  // }, [ref?.current?.offsetHeight]);
   return (
-    <div
-      className={classNames(
-        theme === "light" && "bg-white text-zinc-900" || "bg-zinc-900 text-white",
-        "rounded-md font-mono flex flex-col gap-4 p-4"
-      )}
-    >
-      <ThemeToggle theme={theme} setTheme={setTheme} />
-      {render({state})}
+    <div ref={ref}>
+      <Props state={state} />
     </div>
   );
 }
